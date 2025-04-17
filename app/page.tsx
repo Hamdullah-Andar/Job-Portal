9092edcf3b9ad7b422456ca70447f9a43cbf1fab
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-04-17 17:09:20 +0430

    Initial setup and Clerck Configuration

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+const HomePage = () => {
+  return (
+    <div><Button variant={'destructive'} size={'lg'}>Click ME</Button></div>
+  );
+};
+
+export default HomePage;
