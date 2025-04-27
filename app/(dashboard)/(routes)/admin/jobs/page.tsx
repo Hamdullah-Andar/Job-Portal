21d77612dd6b607399e6260889032d36d5e94c94
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-04-27 23:09:30 +0430

    Add Create job button, Add Job title form

--- app/(dashboard)/(routes)/admin/jobs/page.tsx
+++ app/(dashboard)/(routes)/admin/jobs/page.tsx
@@ -1,5 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+
 const JobsPageOverview = () => {
-  return <div>JobsPageOverview</div>;
+  return (
+    <div className="p-6">
+      <div className="flex items-end justify-end">
+        <Link href={"/admin/create"}>
+          <Button>
+            <Plus className="mr-2 h-4 w-4" />
+            New Job
+          </Button>
+        </Link>
+      </div>
+
+      {/* Data Table */}
+      
+    </div>
+  );
 };
 
 export default JobsPageOverview;
